664a51aa782ce26951a97d7808cb3db739ca7462
Michael Tomcal <michael.tomcal@zendesk.com>
2022-03-09 17:33:06 -0700

    feat(splitter): working fully managed fr conversion

--- packages/grid/src/utils/useSplitterContext.ts
+++ packages/grid/src/utils/useSplitterContext.ts
@@ -6,23 +6,24 @@
  */
 
 import { createContext, useContext } from 'react';
+import { LAYOUT_STATE, DIMENSIONS, UNITS } from './types'
 
 interface ISplitterContext {
-  layoutState: any;
-  setLayoutValue: (id: string, value: number) => void;
+  layoutState: LAYOUT_STATE;
+  setLayoutValue: (dimension: DIMENSIONS, id: number, value: number) => void;
+  getLayoutValue: (dimension: DIMENSIONS, id: number, units?: UNITS) => number;
   totalPanesHeight: number,
   totalPanesWidth: number,
-  totalHeightFractions: number,
-  totalWidthFractions: number,
+  pixelsPerFr: { rows: number, columns: number }
 }
 
 export const SplitterContext = createContext<ISplitterContext>({
   setLayoutValue: () => undefined,
-  layoutState: {},
+  getLayoutValue: () => 0,
+  layoutState: { rows: [] , columns: [] },
   totalPanesHeight: 1,
   totalPanesWidth: 1,
-  totalHeightFractions: 1,
-  totalWidthFractions: 1,
+  pixelsPerFr: { rows: 0, columns: 0 },
 });
 
 /**
